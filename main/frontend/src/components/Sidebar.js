fe6c4a7fe1fb086f7274c4b023028067a0559f8b
JTBolger <112192375+JTBolger@users.noreply.github.com>
2022-10-04 15:39:06 -0500

    Sidenav+
    Tweaked/added some animation to the side-navigation buttons, as well as a label underneath the icon to guide the user. Not all labels are updated to its respective links, but it can be tweaked further.

--- main/frontend/src/components/Sidebar.js
+++ main/frontend/src/components/Sidebar.js
@@ -6,6 +6,7 @@ import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PermPhoneMsgOutlinedIcon from '@mui/icons-material/PermPhoneMsgOutlined';
 import logo from '../assets/images/afterhours-logo.png'
+import { Transform } from '@mui/icons-material';
 export default function Sidebar(props) { 
 
 return (
@@ -17,13 +18,13 @@ return (
 
 <div className='sideitems'>
 
-<a href='./'><i class='bx bxs-dashboard'></i></a>
-<a href='./search'><i class='bx bxs-search'></i></a>
-<a href='./groups'> <i class='bx bxs-group' ></i></a>
-<a href='./messages'><i class='bx bxs-conversation'></i></a>
-<a><i class='bx bxs-calendar'></i></a>
-<a><i class='bx bxs-bookmark-alt'></i></a>
-<a><i class='bx bxs-phone-call'></i></a>
+<a href='./'><i class='bx bxs-dashboard'></i><p class="label">DASHBOARD</p></a>
+<a href='./search'><i class='bx bxs-search'></i><p class="label">SEARCH</p></a>
+<a href='./groups'> <i class='bx bxs-group' ></i><p class="label">GROUPS</p></a>
+<a href='./messages'><i class='bx bxs-conversation'></i><p class="label">MESSAGES</p></a>
+<a><i class='bx bxs-calendar'></i><p class="label">CALENDAR</p></a>
+<a><i class='bx bxs-bookmark-alt'></i><p class="label">[BLANK]</p></a>
+<a><i class='bx bxs-phone-call'></i><p class="label">[BLANK]</p></a>
 </div>
 </div>
  
